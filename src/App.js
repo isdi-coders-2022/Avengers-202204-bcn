9194f3f2bb49c6a6958adeb6392baf517726c9f5
Luc St-Pierre <luc.st-pierre@protonmail.com>
2022-05-02 19:23:41 +0200

    Deleted Button useless button import and import Layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
-import ButtonText from "./components/ButtonTextComponent/ButtonText";
+import Layout from "./components/Layout/Layout";
+import Home from "./pages/Home";
 
 function App() {
   return (
