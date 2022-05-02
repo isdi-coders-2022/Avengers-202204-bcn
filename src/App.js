987cdc588b1c5fb3c7fee0cf352153d7010cbe44
Luc St-Pierre <luc.st-pierre@protonmail.com>
2022-05-02 17:50:19 +0200

    Created layout container and added classes for quick grid approach

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import LayoutComponent from "./components/LayoutComponent/LayoutComponent";
 
 function App() {
-  return <div className="App">Avengers</div>;
+  return (
+    <div className="App">
+      <LayoutComponent>Children components here</LayoutComponent>
+    </div>
+  );
 }
 
 export default App;
