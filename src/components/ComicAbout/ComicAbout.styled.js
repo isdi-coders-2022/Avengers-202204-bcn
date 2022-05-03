842847f175237839a3b63a4c76ebce315711286c
Luc St-Pierre <luc.st-pierre@protonmail.com>
2022-05-03 16:23:11 +0200

    Added test for ComicAbout to render img

--- src/components/ComicAbout/ComicAbout.styled.js
+++ src/components/ComicAbout/ComicAbout.styled.js
@@ -1,10 +1,22 @@
 import styled from "styled-components";
 
 const StyledComicAbout = styled.div`
-  position: relative;
   overflow: hidden;
-  background: no-repeat url(/assets/Alien.jpeg) 50% 0;
-  background-size: cover;
+  position: relative;
+
+  img {
+    object-fit: cover;
+    object-position: 60% top;
+    width: 100%;
+  }
+
+  /* position: relative;
+  overflow: hidden;
+  object-fit: scale-down;
+  /* background-image: ${(props) => `url(/assets/${props.src}) `};
+  background-repeat: no-repeat;
+  background-position: 50% 0;
+  background-size: cover; */
   width: 100%;
   height: 63rem;
 `;
