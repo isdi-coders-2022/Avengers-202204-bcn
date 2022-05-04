e452fe128831978934f00e2aee53d44dfb532bc1
Olga Tsybikova <tsy2701@gmail.com>
2022-05-04 13:07:34 +0200

    Tested AddComic Action Creator by id

--- src/store/actions/comicActionCreator.test.js
+++ src/store/actions/comicActionCreator.test.js
@@ -1,5 +1,13 @@
-describe("hola", () => {
-  describe("hola", () => {
-    test("hola", () => {});
+import { addComicActionCreator } from "./comicActionCreator";
+
+describe("Given an addComicCreator", () => {
+  describe("When it receives an id:2", () => {
+    test("Then it should return an action with type 'add-comic' and comicId=2", () => {
+      const comicId = 2;
+      const expectedAction = { type: "add-comic", comicId: 2 };
+
+      const action = addComicActionCreator(comicId);
+      expect(action).toEqual(expectedAction);
+    });
   });
 });
