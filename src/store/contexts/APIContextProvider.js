116c3e9eb8fc524625fc0126e8af646088779704
Luc St-Pierre <luc.st-pierre@protonmail.com>
2022-05-04 21:44:22 +0200

    Added APIContextProvider and started apiReducer

---
+++ src/store/contexts/APIContextProvider.js
@@ -0,0 +1,15 @@
+import { useReducer } from "react";
+import apiReducer from "../reducers/apiReducer";
+import APIContext from "./APIContext";
+
+const APIContextProvider = ({ children }) => {
+  const [apiStatus, dispatch] = useReducer(apiReducer, { loading: false });
+
+  return (
+    <APIContext.Provider value={(apiStatus, dispatch)}>
+      {children}
+    </APIContext.Provider>
+  );
+};
+
+export default APIContextProvider;
