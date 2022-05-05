7eae18fd51525910b6e0da85fd9efb3a888577b9
Olga Tsybikova <tsy2701@gmail.com>
2022-05-05 21:33:03 +0200

    Changed names of styles files

--- src/components/ComicList/ComicList.js
+++ src/components/ComicList/ComicList.js
@@ -1,17 +1,17 @@
 import { useContext } from "react";
 import ComicContext from "../../store/contexts/ComicContext";
 import Comic from "../Comic/Comic";
-import StyledComicList from "./ComicList.styled";
+import ComicListStyles from "./ComicListStyles";
 
 const ComicList = () => {
   const { comics } = useContext(ComicContext);
 
   return (
-    <StyledComicList className="comic-list">
+    <ComicListStyles className="comic-list">
       {comics.map((comic) => {
         return <Comic key={comic.id} comic={comic} selected={true} />;
       })}
-    </StyledComicList>
+    </ComicListStyles>
   );
 };
 export default ComicList;
