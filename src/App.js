43ba9c8f039fd2f9cdc5615dfcc7698dd3da5f77
Francesco Fabrissin <francescofabrissin.gmail.com>
2022-05-05 16:56:59 +0200

    fixed path in app test

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Layout from "./components/Layout/Layout";
 import About from "./pages/about/About";
 import Navbar from "./components/Navbar/Navbar";
 
-import Wantlist from "./pages/Wantlist/Wantlist";
+import SelectedComicList from "./pages/SelectedComictlist/SelectedComiclist";
 import Home from "./pages/home/Home";
 import Search from "./components/Search/Search";
 import useAPI from "./hooks/useAPI";
@@ -25,7 +25,7 @@ function App() {
           <Route path="/" element={<Navigate to="/home" />} />
           <Route path="/home" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/wantlist" element={<Wantlist />} />
+          <Route path="/wantlist" element={<SelectedComicList />} />
         </Routes>
       </Layout>
       <Search active={false} />
