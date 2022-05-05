24e11f5ebefea02ff785cce9598d0afba95d881c
Olga Tsybikova <tsy2701@gmail.com>
2022-05-05 21:53:45 +0200

    Changed banner to the lighter one

--- src/components/HeaderHero/HeaderHero.js
+++ src/components/HeaderHero/HeaderHero.js
@@ -31,7 +31,7 @@ const HeaderHero = (_title) => {
       )}
       <img
         className="thumbnail"
-        src="/assets/avengers-banner.jpeg"
+        src="/assets/avengers-banner-light.jpeg"
         alt="title"
       />
       <img
