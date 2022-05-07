c9841982c4acbe0952036189e3b4fe4cc4bb63d2
Luc St-Pierre <luc.st-pierre@protonmail.com>
2022-05-08 00:07:57 +0200

    Added set loading action type case

--- src/store/reducers/apiReducer.js
+++ src/store/reducers/apiReducer.js
@@ -1,2 +1,21 @@
-const apiReducer = () => {};
+import actionsTypes from "../actions/api/actionsTypes";
+
+const apiReducer = (apiStatus, action) => {
+  let newApiStatus;
+
+  switch (action.type) {
+    case actionsTypes.setLoading:
+      newApiStatus = {
+        ...apiStatus,
+        loading: true,
+      };
+      break;
+
+    default:
+      newApiStatus = { ...apiStatus };
+  }
+
+  return newApiStatus;
+};
+
 export default apiReducer;
